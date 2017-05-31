f89f7dac57dc8a5bcbc71a32e72aad08ac2deffc
potato-aardvark <potato-aardvark@users.noreply.github.com>
2017-05-31 13:59:01 -0400

    Update gitignore and make tf nnet easier to debug tf nnet now logs output in stdout and is kind of compatible with tensorboard.

--- tfmat.py
+++ tfmat.py
@@ -2,42 +2,98 @@ import tensorflow as tf
 import random as rand
 
 NUM_IN = 2
-NUM_HIDDEN = 10
-NUM_OUT = 1
+NUM_HIDDEN = 5
+NUM_OUT = 2
 
-x = tf.placeholder(tf.float32, [None, NUM_IN], name='x') #inputs
-W1 = tf.Variable(tf.zeros([NUM_IN, NUM_HIDDEN])) #weights of hiddens
-b1 = tf.Variable(tf.zeros([NUM_HIDDEN])) #biases
+def add_summaries(var):
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+    with tf.name_scope('stddev'):
+        stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar('stddev', stddev)
+    tf.summary.scalar('max', tf.reduce_max(var))
+    tf.summary.scalar('min', tf.reduce_min(var))
+    tf.summary.histogram('histogram', var)
 
-H1 = tf.nn.softmax(tf.matmul(x, W1) + b1) #vals of hiddens
-
-W2 = tf.Variable(tf.zeros([NUM_HIDDEN, NUM_OUT])) #weights of outs
-b2 = tf.Variable(tf.zeros([NUM_OUT])) #biases of hiddens
-
-y = tf.matmul(H1, W2) + b2
-
-y_ = tf.placeholder(tf.float32, [None, NUM_OUT], name='true_ys')
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
-def adder_gen(num):
+def operation_gen(num):
     xs = []
     ys = []
     for _ in range(num):
-        a = rand.random()
-        b = rand.random()
+        a = rand.random()/2.0
+        b = rand.random()/2.0
         xs.append((a,b))
-        ys.append((a+b,))
+        ys.append((a+b,a*b))
     return xs, ys
 
-with tf.Session() as sess:
-    tf.global_variables_initializer().run()
-    for _ in range(1000):
-        batch_xs, batch_ys = adder_gen(100)
-        sess.run(train_step, feed_dict={x: batch_xs, y_:batch_ys})
+def stringify_tensors(labels: list, tensors: list, sep='\n'):
+    return sep.join([str(label) + ': ' + str(tensor) for label, tensor in zip(labels, tensors)])
 
-    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    print(sess.run(accuracy, feed_dict={x:[(1,2)] , y_: [(3,)]}))
+def visualize_tensors(labels: list, tensors: list, start='', end='----\n'):
+    return start + stringify_tensors(labels, tensors) + end
+
+with tf.name_scope('in'):
+    x = tf.placeholder(tf.float32, [None, NUM_IN], name='x') #inputs
+    add_summaries(x)
+
+with tf.name_scope('hidden1'):
+    with tf.name_scope('weights'):
+        W1 = tf.Variable(tf.random_uniform([NUM_IN, NUM_HIDDEN], -1.0)) #weights of hiddens
+        add_summaries(W1)
+    with tf.name_scope('biases'):
+        b1 = tf.Variable(tf.random_uniform([NUM_HIDDEN], -1.0)) #biases
+        add_summaries(b1)
+    with tf.name_scope('preacts'):
+        pre1 = tf.matmul(x, W1) + b1 #pre-activations of hiddens
+        add_summaries(pre1)
+    with tf.name_scope('acts'):
+        H1 = tf.sigmoid(pre1) #activations of hiddens
+        add_summaries(H1)
+
+with tf.name_scope('out'):
+    with tf.name_scope('weights'):
+        W2 = tf.Variable(tf.random_uniform([NUM_HIDDEN, NUM_OUT], -1.0)) #weights of outs
+        add_summaries(W2)
+    with tf.name_scope('biases'):
+        b2 = tf.Variable(tf.random_uniform([NUM_OUT], -1.0)) #biases of outs
+        add_summaries(b2)
+    with tf.name_scope('preacts'):        
+        pre2 = tf.sigmoid(tf.matmul(H1, W2) + b2) #pre-activations of outs
+        add_summaries(pre2)
+    with tf.name_scope('acts'):
+        y = tf.identity(pre2) #activations of outs
+        add_summaries(y)
+
+y_ = tf.placeholder(tf.float32, [None, NUM_OUT], name='true_ys')
+#cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+with tf.name_scope('cost'):
+    mse_cost = tf.losses.mean_squared_error(y_, y)
+    add_summaries(mse_cost)
+
+train_step = tf.train.GradientDescentOptimizer(3).minimize(mse_cost)
+
+with tf.Session() as sess:
+    merged_summaries = tf.summary.merge_all()
+    res_writer = tf.summary.FileWriter('./results/results')
+    
+    tf.global_variables_initializer().run()
+    for i in range(50):
+        for j in range(100):
+            batch_xs, batch_ys = operation_gen(10)
+            _, summaries, yr, W2r, b2r, H1r, W1r, b1r, xr = sess.run(
+                (train_step, merged_summaries, y, W2, b2, H1, W1, b1, x),
+                feed_dict={x: batch_xs, y_: batch_ys})
+        res_writer.add_summary(summaries)
+        print(visualize_tensors(
+            ['y', 'W2', 'b2', 'H1', 'W1', 'b1', 'x'],
+            [yr, W2r, b2r, H1r, W1r, b1r, xr]
+            ))
+
+        final_cost = tf.losses.mean_squared_error(y_, y)
+        print('cost after', (i+1)*(j+1), 'iterations:',
+              sess.run(final_cost, feed_dict={x:[(0.1,0.2)] , y_: [(0.3,0.02)]}),
+              'final results:',
+              sess.run(y, feed_dict={x:[(0.1,0.2)] , y_: [(0.3,0.02)]}))
+        
+        
 
