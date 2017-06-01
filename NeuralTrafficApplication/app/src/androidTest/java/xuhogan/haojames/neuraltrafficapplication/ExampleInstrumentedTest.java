c2c114fbe3bfd5776e65e9a7d6caea1e77f72372
potato-aardvark <potato-aardvark@users.noreply.github.com>
2017-06-01 12:08:24 -0400

    Fix AS project

--- NeuralTrafficApplication/app/src/androidTest/java/xuhogan/haojames/neuraltrafficapplication/ExampleInstrumentedTest.java
+++ NeuralTrafficApplication/app/src/androidTest/java/xuhogan/haojames/neuraltrafficapplication/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("haojames.haojames.haojames.haohames.neuraltrafficapplication", appContext.getPackageName());
+        assertEquals("xuhogan.haojames.neuraltrafficapplication", appContext.getPackageName());
     }
 }
